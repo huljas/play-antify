581bd495ab810a9e58daf801c6feef6796f80136
Heikki Uljas <heikki.uljas@hyperin.com>
2010-12-31 00:26:23 +0200

    Improved built-in targets, normalized module paths
    - Fixed auto-test to stop when the tests have been run.
    - Fixed auto-test to trigger code coverage generation manually.
    - Added path normalization which tries to convert module paths
    to relative paths from play.path or basedir

--- commands.py
+++ commands.py
@@ -25,44 +25,34 @@ def execute(**kargs):
         for module in modules:
             if module.endswith("antify"):
                 modAntify = module
-
-        classpath = app.getClasspath()
+        antifyPath = '%s' % modAntify
+        playPath = env["basedir"]
+        basedir = app.path
         application_name = app.readConf('application.name')
 
         buildFile = os.path.join(app.path, 'build.xml')
         shutil.copyfile(os.path.join(modAntify, 'src/buildTemplate.xml'), buildFile)
         cpXML = ""
-
-        replaceAll(buildFile, r'%PLAYHOME%', env["basedir"].replace('\\', '/'))
+        replaceAll(buildFile, r'%PLAYHOME%', playPath.replace('\\', '/'))
         replaceAll(buildFile, r'%APPLICATION_NAME%', application_name)
-
+        parentPath = os.path.normpath(os.path.join(app.path, ".."))
+        replaceAll(buildFile, r'%ANTIFYHOME%', normalizePath(antifyPath, playPath, basedir))
         if len(modules):
             mClasses = ""
-            for module in modules:
-                mClasses += '        <fileset dir="%s/lib" erroronmissingdir="false">\n            <include name="*.jar"/>\n        </fileset>\n' % module
-            replaceAll(buildFile, r'%MODULES_CLASSES%', mClasses)
             mSrc = ""
             for module in modules:
-                mSrc += '        <pathelement path="%s/app"/>\n' % module
+                mClasses += '        <fileset dir="%s/lib" erroronmissingdir="false">\n            <include name="*.jar"/>\n        </fileset>\n' % normalizePath(module, playPath, basedir)
+                mSrc += '        <pathelement path="%s/app"/>\n' % normalizePath(module, playPath, basedir)
+            replaceAll(buildFile, r'%MODULES_CLASSES%', mClasses)
             replaceAll(buildFile, r'%MODULES_SOURCES%', mSrc)
         else:
             replaceAll(buildFile, r'%MODULES_CLASSES%', '')
-            replaceAll(buildFile, r'%MODULES_SOURCES%', '')                                
+            replaceAll(buildFile, r'%MODULES_SOURCES%', '')
 
-# This will be executed before any command (new, run...)
-def before(**kargs):
-    command = kargs.get("command")
-    app = kargs.get("app")
-    args = kargs.get("args")
-    env = kargs.get("env")
-
-
-# This will be executed after any command (new, run...)
-def after(**kargs):
-    command = kargs.get("command")
-    app = kargs.get("app")
-    args = kargs.get("args")
-    env = kargs.get("env")
-
-    if command == "new":
-        pass
+def normalizePath(path, playPath, basedir):
+    basedirParent = os.path.normpath(os.path.join(basedir, ".."))
+    path = path.replace(playPath, '${play.path}')
+    path = path.replace(basedir, '${basedir}')
+    path = path.replace(basedirParent, '${basedir}/..')
+    path = path.replace('\\', '/')
+    return path
